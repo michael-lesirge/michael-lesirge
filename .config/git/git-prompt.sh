b6ffb5e684352faac34e7a9f63a9038cbe7aa0e8
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2022-10-11 19:53:02 -0700

    Update git-prompt.sh

--- .config/git/git-prompt.sh
+++ .config/git/git-prompt.sh
@@ -5,10 +5,10 @@ PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
 PS1="$PS1"'\n'                 # new line
 
 PS1="$PS1"'\[\033[32m\]'       # change to green
-PS1="$PS1"'\u@\h '             # user@host<space>
+PS1="$PS1"'\u ' # @\h '             # user@host<space>
 
-# PS1="$PS1"'\[\033[35m\]'       # change to purple
-# PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
+PS1="$PS1"'\[\033[35m\]'       # change to purple
+PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
 
 # PS1="$PS1"'\[\033[35m\]'       # change to yellow
 # PS1="$PS1"'\@ '                # time as 12:00 AM
